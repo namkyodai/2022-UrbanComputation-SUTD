c3e0d07061c3aa9ba4a240ec544b613b0add2792
Nam LE <namkyodai@users.noreply.github.com>
2022-03-31 10:27:13 +0530

    update postgresql connection

--- notes/sg-houseprice/postgresql-connection.R
+++ notes/sg-houseprice/postgresql-connection.R
@@ -2,8 +2,30 @@ require(DBI)
 require(RPostgres)
 require(RPostgreSQL)
 
-con<-dbConnect(dbDriver("PostgreSQL"), dbname="sghousingprice", host="localhost", port=5432, user="postgres",password="St029477")
+#con<-dbConnect(dbDriver("PostgreSQL"), dbname="hcmc_population2019", host="localhost", port=5432, user="postgres",password="St029477")
+
+
+con<- dbConnect(RPostgres::Postgres(),user="postgres",password="St029477",host="localhost",port=5432,dbname="sghouseprice")
+
+dbListTables(con)
+# Select
+
+df1 <- dbGetQuery(con, "SELECT * FROM communes")
+library(dplyr)
+
+print(df1)
+
+glimpse(df1)
 
 
 
+### below code is for another example (for testing only)
 
+
+df2 <- dbGetQuery(con, "select id as id, geom, ST_Area(geom,false)/1000000 as area, tongdans01/(ST_Area(geom,false)/1000000) as density
+from communes")
+
+
+
+df3 <- dbGetQuery(con, "select id as id, geom, ST_Area(geom,false)/1000000 as area, tongdans01/(ST_Area(geom,false)/1000000) as density
+from communes")
